e82bd4e01e0be3dd969cc54f979570ee8a9bd846
Lionel DUCHATEAU <lionel.duchateau@free.fr>
2024-03-14 15:07:29 +0100

    Updated copyright date.
    Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

--- packages/tools/fileindexer/LSMASHIndexer.cs
+++ packages/tools/fileindexer/LSMASHIndexer.cs
@@ -1,6 +1,6 @@
 ﻿// ****************************************************************************
 // 
-// Copyright (C) 2005-2022 Doom9 & al
+// Copyright (C) 2005-2024 Doom9 & al
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
