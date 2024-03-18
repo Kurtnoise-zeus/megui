7770412482b9357c8e9acb06242ac15f73683c32
Lionel DUCHATEAU <lionel.duchateau@free.fr>
2024-03-18 14:22:03 +0100

    Minor version as current date.
    Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -46,8 +46,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.6666.1503")]
-[assembly: AssemblyFileVersion("1.0.6666.1503")]
+[assembly: AssemblyVersion("1.0.6666.1803")]
+[assembly: AssemblyFileVersion("1.0.6666.1803")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
