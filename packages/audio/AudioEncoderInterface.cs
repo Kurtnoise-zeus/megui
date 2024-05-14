f3b6d728bb04d6482c7249d53a220d5cd5feea3a
Lionel DUCHATEAU <lionel.duchateau@free.fr>
2024-05-14 11:47:37 +0200

    Get Channel Mask Values from Channel Layout instead.
    Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

--- packages/audio/AudioEncoderInterface.cs
+++ packages/audio/AudioEncoderInterface.cs
@@ -202,6 +202,50 @@ namespace MeGUI
             return mask;
         }
 
+        private static uint GetSMask(string chLayout)
+        {
+            uint m = 0;
+            string[] cll = chLayout.Split();
+
+            foreach (var cl in cll)
+            {
+                m += (String.IsNullOrEmpty(cl) ? 3 : MaskValue(cl)); // assume Stereo if chLayout is empty
+            }
+
+            return m;
+        }
+
+        private static uint MaskValue(string chLayout)
+        {
+            uint m = 0;
+
+            switch (chLayout)
+            {
+                case "L": m += 0x1; break;
+                case "R": m += 0x2; break;
+                case "C": m += 0x4; break;
+                case "LFE": m += 0x8; break;
+                case "Bfl": m += 0x10; break;
+                case "Bfr": m += 0x20; break;
+                case "Lc": m += 0x40; break;
+                case "Rc": m += 0x80; break;
+                case "Cb": m += 0x100; break;
+                case "Ls": m += 0x200; break;
+                case "Rs": m += 0x400; break;
+                case "Tc": m += 0x800; break;
+                case "Tfl": m += 0x1000; break;
+                case "Tfc": m += 0x2000; break;
+                case "Tfr": m += 0x4000; break;
+                case "Tbl": m += 0x8000; break;
+                case "Tbc": m += 0x10000; break;
+                case "Tbr": m += 0x20000; break;
+                default: m += 0; break;
+
+            }
+
+            return m;
+        }
+
         private void CreateTemporallyEqFiles(string tempPath)
         {
             // http://forum.doom9.org/showthread.php?p=778156#post778156
@@ -1202,7 +1246,7 @@ namespace MeGUI
 
                 if (iChannelCount != 0)
                 {
-                    _iChannelMask = (int)GetSpeakerMask(iChannelCount);
+                    _iChannelMask = (int)GetSMask(strChannelPositions);
                     if (_iChannelMask != 0)
                     {
                         script.AppendFormat(@"# Detected Channel Mask: cm={0}{1}", _iChannelMask.ToString(), Environment.NewLine);
