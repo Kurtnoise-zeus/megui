069034db58a5b0be24f1a1c78943583750298c30
Lionel DUCHATEAU <lionel.duchateau@free.fr>
2024-08-19 12:54:19 +0200

    Added initial components for svt-av1-psy encoder interface.
    Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

---
+++ packages/video/svtav1psy/svtav1psyConfigurationPanel.Designer.cs
@@ -0,0 +1,358 @@
+// ****************************************************************************
+// 
+// Copyright (C) 2005-2024 Doom9 & al
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ****************************************************************************
+
+namespace MeGUI.packages.video.svtav1psy
+{
+    partial class svtav1psyConfigurationPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customCommandlineOptionsLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.helpButton1 = new MeGUI.core.gui.HelpButton();
+            this.MiscTabPage = new System.Windows.Forms.TabPage();
+            this.gbffv1CustomCmd = new System.Windows.Forms.GroupBox();
+            this.customCommandlineOptions = new System.Windows.Forms.TextBox();
+            this.ffv1ThreadsLabel = new System.Windows.Forms.Label();
+            this.ffv1NbThreads = new System.Windows.Forms.NumericUpDown();
+            this.ffv1CodecGeneralGroupbox = new System.Windows.Forms.GroupBox();
+            this.svtEncodingMode = new System.Windows.Forms.ComboBox();
+            this.svtBitrateQuantizer = new System.Windows.Forms.NumericUpDown();
+            this.svtBitrateQuantizerLabel = new System.Windows.Forms.Label();
+            this.gbPresets = new System.Windows.Forms.GroupBox();
+            this.lbPreset = new System.Windows.Forms.Label();
+            this.tbsvtPresets = new System.Windows.Forms.TrackBar();
+            this.gbTunes = new System.Windows.Forms.GroupBox();
+            this.svtTunes = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.mainTabPage.SuspendLayout();
+            this.MiscTabPage.SuspendLayout();
+            this.gbffv1CustomCmd.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ffv1NbThreads)).BeginInit();
+            this.ffv1CodecGeneralGroupbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.svtBitrateQuantizer)).BeginInit();
+            this.gbPresets.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbsvtPresets)).BeginInit();
+            this.gbTunes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.MiscTabPage);
+            this.tabControl1.Size = new System.Drawing.Size(370, 246);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Controls.SetChildIndex(this.MiscTabPage, 0);
+            this.tabControl1.Controls.SetChildIndex(this.mainTabPage, 0);
+            // 
+            // commandline
+            // 
+            this.commandline.Location = new System.Drawing.Point(0, 248);
+            this.commandline.Size = new System.Drawing.Size(367, 89);
+            this.commandline.TabIndex = 1;
+            this.commandline.Text = " ";
+            // 
+            // mainTabPage
+            // 
+            this.mainTabPage.Controls.Add(this.gbTunes);
+            this.mainTabPage.Controls.Add(this.gbPresets);
+            this.mainTabPage.Controls.Add(this.ffv1CodecGeneralGroupbox);
+            this.mainTabPage.Controls.Add(this.helpButton1);
+            this.mainTabPage.Controls.Add(this.ffv1ThreadsLabel);
+            this.mainTabPage.Controls.Add(this.ffv1NbThreads);
+            this.mainTabPage.Size = new System.Drawing.Size(362, 220);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Intra luma quantization deadzone";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Inter luma quantization deadzone";
+            // 
+            // customCommandlineOptionsLabel
+            // 
+            this.customCommandlineOptionsLabel.Location = new System.Drawing.Point(6, 301);
+            this.customCommandlineOptionsLabel.Name = "customCommandlineOptionsLabel";
+            this.customCommandlineOptionsLabel.Size = new System.Drawing.Size(167, 13);
+            this.customCommandlineOptionsLabel.TabIndex = 1;
+            this.customCommandlineOptionsLabel.Text = "Custom Commandline Options";
+            this.customCommandlineOptionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // helpButton1
+            // 
+            this.helpButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.helpButton1.ArticleName = "Configuration/Video_Encoder_Configuration/SVTAV1PSY";
+            this.helpButton1.AutoSize = true;
+            this.helpButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.helpButton1.Location = new System.Drawing.Point(299, 154);
+            this.helpButton1.Name = "helpButton1";
+            this.helpButton1.Size = new System.Drawing.Size(39, 23);
+            this.helpButton1.TabIndex = 10;
+            // 
+            // MiscTabPage
+            // 
+            this.MiscTabPage.Controls.Add(this.gbffv1CustomCmd);
+            this.MiscTabPage.Location = new System.Drawing.Point(4, 22);
+            this.MiscTabPage.Name = "MiscTabPage";
+            this.MiscTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.MiscTabPage.Size = new System.Drawing.Size(362, 220);
+            this.MiscTabPage.TabIndex = 6;
+            this.MiscTabPage.Text = "Misc";
+            this.MiscTabPage.UseVisualStyleBackColor = true;
+            // 
+            // gbffv1CustomCmd
+            // 
+            this.gbffv1CustomCmd.Controls.Add(this.customCommandlineOptions);
+            this.gbffv1CustomCmd.Location = new System.Drawing.Point(6, 6);
+            this.gbffv1CustomCmd.Name = "gbffv1CustomCmd";
+            this.gbffv1CustomCmd.Size = new System.Drawing.Size(490, 65);
+            this.gbffv1CustomCmd.TabIndex = 27;
+            this.gbffv1CustomCmd.TabStop = false;
+            this.gbffv1CustomCmd.Text = " Custom Command Line ";
+            // 
+            // customCommandlineOptions
+            // 
+            this.customCommandlineOptions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customCommandlineOptions.Location = new System.Drawing.Point(15, 19);
+            this.customCommandlineOptions.Multiline = true;
+            this.customCommandlineOptions.Name = "customCommandlineOptions";
+            this.customCommandlineOptions.Size = new System.Drawing.Size(464, 34);
+            this.customCommandlineOptions.TabIndex = 0;
+            // 
+            // ffv1ThreadsLabel
+            // 
+            this.ffv1ThreadsLabel.AutoSize = true;
+            this.ffv1ThreadsLabel.Location = new System.Drawing.Point(15, 154);
+            this.ffv1ThreadsLabel.Name = "ffv1ThreadsLabel";
+            this.ffv1ThreadsLabel.Size = new System.Drawing.Size(95, 13);
+            this.ffv1ThreadsLabel.TabIndex = 17;
+            this.ffv1ThreadsLabel.Text = "Threads (0 = Auto)";
+            // 
+            // ffv1NbThreads
+            // 
+            this.ffv1NbThreads.Location = new System.Drawing.Point(117, 152);
+            this.ffv1NbThreads.Name = "ffv1NbThreads";
+            this.ffv1NbThreads.Size = new System.Drawing.Size(43, 20);
+            this.ffv1NbThreads.TabIndex = 18;
+            this.ffv1NbThreads.ValueChanged += new System.EventHandler(this.updateEvent);
+            // 
+            // ffv1CodecGeneralGroupbox
+            // 
+            this.ffv1CodecGeneralGroupbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ffv1CodecGeneralGroupbox.Controls.Add(this.svtBitrateQuantizer);
+            this.ffv1CodecGeneralGroupbox.Controls.Add(this.svtEncodingMode);
+            this.ffv1CodecGeneralGroupbox.Controls.Add(this.svtBitrateQuantizerLabel);
+            this.ffv1CodecGeneralGroupbox.Location = new System.Drawing.Point(6, 6);
+            this.ffv1CodecGeneralGroupbox.Name = "ffv1CodecGeneralGroupbox";
+            this.ffv1CodecGeneralGroupbox.Size = new System.Drawing.Size(231, 48);
+            this.ffv1CodecGeneralGroupbox.TabIndex = 46;
+            this.ffv1CodecGeneralGroupbox.TabStop = false;
+            this.ffv1CodecGeneralGroupbox.Text = " Encoding Mode ";
+            // 
+            // svtEncodingMode
+            // 
+            this.svtEncodingMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.svtEncodingMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.svtEncodingMode.Items.AddRange(new object[] {
+            "None",
+            "2pass - 1st pass",
+            "2pass - 2nd pass",
+            "Automated 2pass"});
+            this.svtEncodingMode.Location = new System.Drawing.Point(6, 19);
+            this.svtEncodingMode.MaxDropDownItems = 2;
+            this.svtEncodingMode.Name = "svtEncodingMode";
+            this.svtEncodingMode.Size = new System.Drawing.Size(106, 21);
+            this.svtEncodingMode.TabIndex = 2;
+            // 
+            // svtBitrateQuantizer
+            // 
+            this.svtBitrateQuantizer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.svtBitrateQuantizer.Location = new System.Drawing.Point(170, 20);
+            this.svtBitrateQuantizer.Maximum = new decimal(new int[] {
+            300000,
+            0,
+            0,
+            0});
+            this.svtBitrateQuantizer.Name = "svtBitrateQuantizer";
+            this.svtBitrateQuantizer.Size = new System.Drawing.Size(55, 20);
+            this.svtBitrateQuantizer.TabIndex = 48;
+            // 
+            // svtBitrateQuantizerLabel
+            // 
+            this.svtBitrateQuantizerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.svtBitrateQuantizerLabel.Location = new System.Drawing.Point(118, 17);
+            this.svtBitrateQuantizerLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.svtBitrateQuantizerLabel.Name = "svtBitrateQuantizerLabel";
+            this.svtBitrateQuantizerLabel.Padding = new System.Windows.Forms.Padding(3);
+            this.svtBitrateQuantizerLabel.Size = new System.Drawing.Size(46, 23);
+            this.svtBitrateQuantizerLabel.TabIndex = 47;
+            this.svtBitrateQuantizerLabel.Text = "Bitrate";
+            this.svtBitrateQuantizerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // gbPresets
+            // 
+            this.gbPresets.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbPresets.BackColor = System.Drawing.Color.Transparent;
+            this.gbPresets.Controls.Add(this.lbPreset);
+            this.gbPresets.Controls.Add(this.tbsvtPresets);
+            this.gbPresets.Location = new System.Drawing.Point(6, 59);
+            this.gbPresets.Name = "gbPresets";
+            this.gbPresets.Size = new System.Drawing.Size(350, 77);
+            this.gbPresets.TabIndex = 47;
+            this.gbPresets.TabStop = false;
+            this.gbPresets.Text = " Preset ";
+            // 
+            // lbPreset
+            // 
+            this.lbPreset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbPreset.AutoSize = true;
+            this.lbPreset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPreset.Location = new System.Drawing.Point(174, 16);
+            this.lbPreset.Name = "lbPreset";
+            this.lbPreset.Size = new System.Drawing.Size(44, 13);
+            this.lbPreset.TabIndex = 1;
+            this.lbPreset.Text = "Medium";
+            // 
+            // tbsvtPresets
+            // 
+            this.tbsvtPresets.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbsvtPresets.AutoSize = false;
+            this.tbsvtPresets.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbsvtPresets.Location = new System.Drawing.Point(6, 32);
+            this.tbsvtPresets.Maximum = 9;
+            this.tbsvtPresets.Name = "tbsvtPresets";
+            this.tbsvtPresets.Size = new System.Drawing.Size(326, 30);
+            this.tbsvtPresets.TabIndex = 0;
+            this.tbsvtPresets.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.tbsvtPresets.Value = 5;
+            // 
+            // gbTunes
+            // 
+            this.gbTunes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbTunes.Controls.Add(this.svtTunes);
+            this.gbTunes.Location = new System.Drawing.Point(243, 6);
+            this.gbTunes.Name = "gbTunes";
+            this.gbTunes.Size = new System.Drawing.Size(113, 48);
+            this.gbTunes.TabIndex = 48;
+            this.gbTunes.TabStop = false;
+            this.gbTunes.Text = " Tuning ";
+            // 
+            // svtTunes
+            // 
+            this.svtTunes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.svtTunes.FormattingEnabled = true;
+            this.svtTunes.Location = new System.Drawing.Point(10, 19);
+            this.svtTunes.Name = "svtTunes";
+            this.svtTunes.Size = new System.Drawing.Size(97, 21);
+            this.svtTunes.TabIndex = 0;
+            // 
+            // svtav1psyConfigurationPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Name = "svtav1psyConfigurationPanel";
+            this.Size = new System.Drawing.Size(370, 340);
+            this.tabControl1.ResumeLayout(false);
+            this.mainTabPage.ResumeLayout(false);
+            this.mainTabPage.PerformLayout();
+            this.MiscTabPage.ResumeLayout(false);
+            this.gbffv1CustomCmd.ResumeLayout(false);
+            this.gbffv1CustomCmd.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ffv1NbThreads)).EndInit();
+            this.ffv1CodecGeneralGroupbox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.svtBitrateQuantizer)).EndInit();
+            this.gbPresets.ResumeLayout(false);
+            this.gbPresets.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbsvtPresets)).EndInit();
+            this.gbTunes.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label customCommandlineOptionsLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private MeGUI.core.gui.HelpButton helpButton1;
+        private System.Windows.Forms.TabPage MiscTabPage;
+        private System.Windows.Forms.GroupBox gbffv1CustomCmd;
+        private System.Windows.Forms.TextBox customCommandlineOptions;
+        private System.Windows.Forms.Label ffv1ThreadsLabel;
+        private System.Windows.Forms.NumericUpDown ffv1NbThreads;
+        private System.Windows.Forms.GroupBox ffv1CodecGeneralGroupbox;
+        private System.Windows.Forms.ComboBox svtEncodingMode;
+        private System.Windows.Forms.NumericUpDown svtBitrateQuantizer;
+        private System.Windows.Forms.Label svtBitrateQuantizerLabel;
+        private System.Windows.Forms.GroupBox gbPresets;
+        private System.Windows.Forms.Label lbPreset;
+        private System.Windows.Forms.TrackBar tbsvtPresets;
+        private System.Windows.Forms.GroupBox gbTunes;
+        private System.Windows.Forms.ComboBox svtTunes;
+    }
+}
---
+++ packages/video/svtav1psy/svtav1psyConfigurationPanel.cs
@@ -0,0 +1,199 @@
+// ****************************************************************************
+// 
+// Copyright (C) 2005-2024 Doom9 & al
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+using MeGUI.core.details.video;
+using MeGUI.core.gui;
+using MeGUI.core.plugins.interfaces;
+
+namespace MeGUI.packages.video.svtav1psy
+{
+    public partial class svtav1psyConfigurationPanel : MeGUI.core.details.video.VideoConfigurationPanel, Editable<svtav1psySettings>
+    {
+        private XmlDocument ContextHelp = new XmlDocument();
+        public svtav1psyConfigurationPanel()
+            : base()
+        {
+            InitializeComponent();
+            svtEncodingMode.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Returns whether the given mode is a bitrate or quality-based mode
+        /// </summary>
+        /// <param name="mode">selected encoding mode</param>
+        /// <returns>true if the mode is a bitrate mode, false otherwise</returns>
+        private bool isBitrateMode(VideoCodecSettings.VideoEncodingMode mode)
+        {
+            return !(mode == VideoCodecSettings.VideoEncodingMode.none);
+        }
+        private void doDropDownAdjustments()
+        {
+            //lastFFV1EncodingMode = (VideoCodecSettings.FFV1EncodingMode)this.ffv1EncodingMode.SelectedIndex;
+        }
+
+        protected override string getCommandline()
+        {
+            ulong x = 1;
+            return svtav1psyEncoder.genCommandline(null, null, null, -1, -1, ref x, Settings as svtav1psySettings, null);
+        }
+        /// <summary>
+        /// Does all the necessary adjustments after a GUI change has been made.
+        /// </summary>
+        protected override void doCodecSpecificAdjustments()
+        {
+            doDropDownAdjustments();
+        }
+
+        /// <summary>
+        /// Returns whether settings is ffv1Settings
+        /// </summary>
+        /// <param name="settings">The settings to check</param>
+        /// <returns>Whether the settings are valid</returns>
+        protected override bool isValidSettings(VideoCodecSettings settings)
+        {
+            return settings is svtav1psySettings;
+        }
+
+        /// <summary>
+        /// Returns a new instance of ffv1Settings.
+        /// </summary>
+        /// <returns>A new instance of ffv1Settings</returns>
+        protected override VideoCodecSettings defaultSettings()
+        {
+            return new svtav1psySettings();
+        }
+
+        /// <summary>
+        /// gets / sets the settings currently displayed on the GUI
+        /// </summary>
+        public svtav1psySettings Settings
+        {
+            get
+            {
+                svtav1psySettings xs = new svtav1psySettings();
+
+                return xs;
+            }
+            set
+            {
+                // Warning! The ordering of components matters because of the dependency code!
+                if (value == null)
+                    return;
+
+                svtav1psySettings xs = value;
+                updating = true;
+                updating = false;
+                genericUpdate();
+            }
+        }
+        private void updateEvent(object sender, EventArgs e)
+        {
+            genericUpdate();
+        }
+        private void textField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && (int)Keys.Back != (int)e.KeyChar)
+                e.Handled = true;
+        }
+
+        private string SelectHelpText(string node)
+        {
+            StringBuilder HelpText = new StringBuilder(64);
+
+            try
+            {
+                string xpath = "/ContextHelp/Codec[@name='SVTAV1PSY']/" + node;
+                XmlNodeList nl = ContextHelp.SelectNodes(xpath); // Return the details for the specified node
+
+                if (nl.Count == 1) // if it finds the required HelpText, count should be 1
+                {
+                    HelpText.Append(nl[0].Attributes["name"].Value);
+                    HelpText.AppendLine();
+                    HelpText.AppendLine(nl[0]["Text"].InnerText);
+                    HelpText.AppendLine();
+                    HelpText.AppendLine("Default : " + nl[0]["Default"].InnerText);
+                    HelpText.AppendLine("Recommended : " + nl[0]["Recommended"].InnerText);
+                }
+                else // If count isn't 1, then theres no valid data.
+                    HelpText.Append("Error: No data available");
+            }
+            catch
+            {
+                HelpText.Append("Error: No data available");
+            }
+
+            return (HelpText.ToString());
+        }
+
+        private void chMultiPass_CheckedChanged(object sender, EventArgs e)
+        {
+           updateEvent(sender, e);
+        }
+
+        private void chErrorCorrection_CheckedChanged(object sender, EventArgs e)
+        {
+           updateEvent(sender, e);
+        }
+
+        private void chContext_CheckedChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void ch10BitsEncoder_CheckedChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void ffv1Coder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void ffv1Slices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void nmGOPSize_ValueChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void ffv1NbThreads_ValueChanged(object sender, EventArgs e)
+        {
+            updateEvent(sender, e);
+        }
+
+        private void ffv1EncodingMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            genericUpdate();
+        }
+    }
+}
