da897adab67defa64ee20486d6720ad8ae0f0377
Lionel DUCHATEAU <lionel.duchateau@free.fr>
2024-11-18 06:56:04 +0100

    updated 7.1 & 6.1 avs downmix functions. SoxFilter 2.2.xx required

--- packages/audio/AudioEncoderInterface.cs
+++ packages/audio/AudioEncoderInterface.cs
@@ -2021,7 +2021,8 @@ function c71_c51(clip a)
      front = GetChannel(a, 1, 2, 3, 4)
      back  = GetChannel(a, 5, 6)
      side  = GetChannel(a, 7, 8)
-     mix   = MixAudio(back, side, 1.0, 1.0).SoftClipperFromAudX(0.0)
+     mix   = MixAudio(back, side, 0.5, 0.5).ConvertAudioTo32bit()
+     mix   = mix.SoxFilter(""compand 0.1,0.1 -90,-84,-16,-10,-0.1,-3 0.0 -90 0.0"").ConvertAudioToFloat()
      return MergeChannels(front, mix)
   }
 # 6.1 Channels L,R,C,LFE,BC,SL,SR -> standard 5.1
@@ -2031,7 +2032,8 @@ function c61_c51(clip a)
      bcent = GetChannel(a, 5).Amplify(0.7071)
      back  = MergeChannels(bcent, bcent)
      side  = GetChannel(a, 6, 7)
-     mix   = MixAudio(back, side, 1.0, 1.0).SoftClipperFromAudX(0.0)
+     mix   = MixAudio(back, side, 0.5, 0.5).ConvertAudioTo32bit()
+     mix   = mix.SoxFilter(""compand 0.1,0.1 -90,-84,-16,-10,-0.1,-3 0.0 -90 0.0"").ConvertAudioToFloat()
      return MergeChannels(front, mix)
   }"
 );
