Kurtnoise-zeus/megui

commit ad9c009504142dbded00801a2c03d10d124aa9b5

Fixed missing parenthesis for TimeStretch function. #37
Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

packages/audio/AudioEncoderInterface.cs
@@ -2141,13 +2141,13 @@ function c3_dpl2(clip a)
                 case TimeModificationMode.KeepOriginal:
                     break;
                 case TimeModificationMode.SpeedUp23976To24:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "AssumeSampleRate(Round((AudioRate()*1001.0)/100.0)).SSRC(AudioRate())" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(AssumeSampleRate(Round((AudioRate()*1001.0)/100.0)).SSRC(AudioRate()))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp23976To25:
                     script.Append("AssumeSampleRate(Round((AudioRate()*1001.0)/960.0)).SSRC(AudioRate())" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown24To23976:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "SSRC(Round((AudioRate() * 1001.0) / 1000.0)).AssumeSampleRate(AudioRate())" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(SSRC(Round((AudioRate() * 1001.0) / 1000.0)).AssumeSampleRate(AudioRate()))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown25To23976:
                     script.Append("SSRC(Round((AudioRate()*1001.0)/960.0)).AssumeSampleRate(AudioRate())" + Environment.NewLine);
@@ -2159,22 +2159,22 @@ function c3_dpl2(clip a)
                     script.Append("SSRC(Round((AudioRate()*25.0)/24.0)).AssumeSampleRate(AudioRate())" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp23976To24WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "AssumeSampleRate(Round((AudioRate()*1001.0)/100.0)).SSRC(AudioRate())" + Environment.NewLine + "TimeStretch(tempo=(1001.0/10.0))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(AssumeSampleRate(Round((AudioRate()*1001.0)/100.0)).SSRC(AudioRate()))" + Environment.NewLine + "TimeStretch(tempo=(1001.0/10.0))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp23976To25WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(1001.0/9.6))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(1001.0/9.6)))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown25To23976WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(96.0/1.001))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(96.0/1.001)))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp24To25WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(2500.0/24.0))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(2500.0/24.0)))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown25To24WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(2400.0/25.0))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(2400.0/25.0)))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown24To23976WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "SSRC(Round((AudioRate() * 1001.0) / 1000.0)).AssumeSampleRate(AudioRate())" + Environment.NewLine + "TimeStretch(tempo = (100.0 / 1.001))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(SSRC(Round((AudioRate() * 1001.0) / 1000.0)).AssumeSampleRate(AudioRate()))" + Environment.NewLine + "TimeStretch(tempo = (100.0 / 1.001))" + Environment.NewLine);
                     break;
             }
 

commit 769dc43db74184502f831099ff18300b6b677ec2

Fixed issue #38 - Regression from ad9c009
Signed-off-by: Lionel DUCHATEAU <lionel.duchateau@free.fr>

packages/audio/AudioEncoderInterface.cs
@@ -2161,16 +2161,16 @@ function c3_dpl2(clip a)
                     script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(AssumeSampleRate(Round((AudioRate()*1001.0)/100.0)).SSRC(AudioRate()))" + Environment.NewLine + "TimeStretch(tempo=(1001.0/10.0))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp23976To25WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(1001.0/9.6)))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(1001.0/9.6))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown25To23976WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(96.0/1.001)))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(96.0/1.001))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SpeedUp24To25WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(2500.0/24.0)))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(2500.0/24.0))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown25To24WithCorrection:
-                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "((tempo=(2400.0/25.0)))" + Environment.NewLine);
+                    script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(tempo=(2400.0/25.0))" + Environment.NewLine);
                     break;
                 case TimeModificationMode.SlowDown24To23976WithCorrection:
                     script.Append((MainForm.Instance.Settings.AviSynthPlus ? "TimeStretch" : "TimeStretchPlugin") + "(SSRC(Round((AudioRate() * 1001.0) / 1000.0)).AssumeSampleRate(AudioRate()))" + Environment.NewLine + "TimeStretch(tempo = (100.0 / 1.001))" + Environment.NewLine);