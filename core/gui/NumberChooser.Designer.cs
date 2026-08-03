75a3aad114ca970c0c398c028b47263790fb4215
gashtal <mitalkubau@gmail.com>
2026-08-02 19:07:00 -0700

    Launch the "Go to frame" window using CTRL + G regardless of where the focus is inside the video preview window Also close this window using ESC This now mimics the behavior of the keyboard shortcuts in VirtualDub and MPC-HC

--- core/gui/NumberChooser.Designer.cs
+++ core/gui/NumberChooser.Designer.cs
@@ -120,6 +120,7 @@ namespace MeGUI.core.gui
             // NumberChooser
             // 
             this.AcceptButton = this.button2;
+            this.CancelButton = button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(368, 149);
